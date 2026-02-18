1fc9b04263a1da6c685c674cac7c5105f876cf54
labcabrera <lab.cabrera@gmail.com>
2026-02-18 13:02:57 +0100

    Initial modules.

--- modules/kong/install-kong.sh
+++ modules/kong/install-kong.sh
@@ -1,45 +1,23 @@
 #!/bin/bash
 
-# Install Gateway API CRDs
-#kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
+helm repo add kong https://charts.konghq.com
 
-# Create Kong namespace
-#kubectl create namespace kong
+helm repo update
 
+kubectl create namespace kong
 
-kubectl apply -f - <<'YAML'
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: kong
----
-apiVersion: gateway.networking.k8s.io/v1
-kind: GatewayClass
-metadata:
-  name: kong
-  annotations:
-    konghq.com/gatewayclass-unmanaged: "true"
-spec:
-  controllerName: konghq.com/kic-gateway-controller
----
-apiVersion: gateway.networking.k8s.io/v1
-kind: Gateway
-metadata:
-  name: kong
-  namespace: kong
-spec:
-  gatewayClassName: kong
-  listeners:
-  - name: http
-    port: 80
-    protocol: HTTP
-    allowedRoutes:
-      namespaces:
-        from: All
+helm install kong kong/ingress -n kong
+
+# Para local sin LB:
+
+cat > values.yaml <<'YAML'
+gateway:
+  proxy:
+    type: NodePort
 YAML
+
+helm upgrade --install kong kong/ingress -n kong -f values.yaml
 
 # Check
 kubectl get pods -n kong
 kubectl get svc -n kong
-
-#helm upgrade --install kong kong/kong -n kong -f config/values.yaml
